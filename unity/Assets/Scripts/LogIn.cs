8dc39cb84616e78a33de069f3ab139a8fcc12ce9
Spencer Saxon <seniorsaxon@hotmail.com>
2017-12-07 23:02:23 -0500

    Login Functionality

---
+++ unity/Assets/Scripts/LogIn.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class LogIn : MonoBehaviour {
+
+    // Use this for initialization
+
+    public Button btn;
+    public Toggle tgl;
+    public GameObject wrntxt;
+    public InputField username;
+    public InputField password;
+
+    void Start() {
+
+        btn.onClick.AddListener(TaskOnClick);
+
+    }
+
+    void TaskOnClick()
+    {
+
+        if (username.text.Length <= 0 && password.text.Length <= 0)
+        {
+            wrntxt.SetActive(true);
+        }
+        else
+        {
+            wrntxt.SetActive(false);
+        }
+        
+
+        if(username.text.Length >=0 && password.text.Length >= 0)
+        {
+            StartCoroutine(Upload());
+        }
+        
+
+        if (tgl.isOn == false)
+        {
+            username.text = "";
+            password.text = "";
+        }
+
+        
+
+        
+
+
+    }
+
+
+
+
+    IEnumerator Upload() {
+		WWWForm form = new WWWForm();
+		form.AddField( "username", username.text );
+		form.AddField( "password", password.text );
+		byte[] rawData = form.data;
+		string url = "http://localhost:3001/account/login";
+
+		// Post a request to an URL with our custom headers
+		WWW www = new WWW(url, rawData);
+		yield return www;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+    }
+}
---
+++ unity/Assets/Scripts/SignUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SignUp : MonoBehaviour {
+
+
+    public Button btn;
+
+
+
+	// Use this for initialization
+	void Start () {
+        btn.onClick.AddListener(TaskOnClick);
+    }
+	
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("signup");
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
