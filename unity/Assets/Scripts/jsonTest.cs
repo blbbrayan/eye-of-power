4944702d1b6ee7257a37607305885ecc5b75e769
Brayan <brayanbyrdsong@gmail.com>
2017-12-09 14:18:49 -0500

    Added JSON Test Script

---
+++ unity/Assets/Scripts/jsonTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jsonTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		//example json string (like the one from the server)
+		var jsonString = "{\"name\": \"Steve\", \"age\": 42}";
+		//looks like this {name: "Steve", age: 42}
+
+
+		//how to turn it into a json
+		JSONObject json = new JSONObject(jsonString);
+
+		//how to access the data
+		Debug.Log("name: " + json.GetField("name")); //gets the data of name
+		Debug.Log("age: " + json.GetField("age")); //gets the data of age
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
